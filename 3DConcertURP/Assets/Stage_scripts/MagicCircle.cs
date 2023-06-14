fac494a4c0a1767fb34d09d95948894b41587b68
PinkParrot011 <nattamonchu@gmail.com>
2023-06-14 14:29:21 -0700

    Tidying up scene
    - Just figure out how to blend two post processing profile on the same scene
    - delete the 'above dome' scene
    - tidy up GameObjects in the scene (renaming, moving)

--- 3DConcertURP/Assets/Stage_scripts/MagicCircle.cs
+++ 3DConcertURP/Assets/Stage_scripts/MagicCircle.cs
@@ -11,27 +11,51 @@ namespace SonicBloom.Koreo.Demos
         [EventID]
         public string eventMagicCircle;
 
+        [EventID]
+        public string eventSPLights;
+
+        [SerializeField]
+        GameObject aboveLights;
+
         [SerializeField]
         Transform performer;
 
         [SerializeField]
         Transform OutsideDome;
 
+        [SerializeField]
+        Light[] SP;
+
         float t;
         Vector3 newPos;
         Vector3 originalPos;
+        float originalIntensity;
+        float fadeT;
 
         void Start()
         {
             Koreographer.Instance.RegisterForEventsWithTime(eventMagicCircle, MagicEvent);
+            Koreographer.Instance.RegisterForEventsWithTime(eventSPLights, FadeEvent);
             AttachMagicCircle();
             originalPos = this.transform.position;
+
+            originalIntensity = SP[0].intensity;
         }
 
+        void FadeEvent(KoreographyEvent evt, int sampleTime, int sampleDelta, DeltaSlice deltaSlice)
+        {
+            if (evt.HasCurvePayload())
+            {
+                fadeT = evt.GetValueOfCurveAtTime(sampleTime);
+                for (int i = 0; i < SP.Length; i++)
+                    SP[i].intensity = Mathf.Lerp(0.0f, originalIntensity, fadeT);
+            }
+        }
         void MagicEvent(KoreographyEvent evt, int sampleTime, int sampleDelta, DeltaSlice deltaSlice)
         {
             if (evt.HasCurvePayload())
             {
+                aboveLights.SetActive(true);
                 //calc new position using lerp
                 t = evt.GetValueOfCurveAtTime(sampleTime);
 
