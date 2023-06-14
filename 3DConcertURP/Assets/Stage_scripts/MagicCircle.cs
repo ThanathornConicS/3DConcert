5f32be4c81d587dd3a1d91f440cc7a0aa266dd8b
PinkParrot011 <nattamonchu@gmail.com>
2023-06-13 20:45:44 -0700

    Fine tunes
    - Implement magic circle
    - delete unnecessary stuffs

--- 3DConcertURP/Assets/Stage_scripts/MagicCircle.cs
+++ 3DConcertURP/Assets/Stage_scripts/MagicCircle.cs
@@ -3,28 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
 
-public class MagicCircle : MonoBehaviour
+
+namespace SonicBloom.Koreo.Demos
 {
-    [SerializeField]
-    Transform performer;
-
-    [Button]
-    void AttachMagicCircle()
+    public class MagicCircle : MonoBehaviour
     {
-        performer.SetParent(this.transform, true);
-    }
+        [EventID]
+        public string eventMagicCircle;
 
-    [Button]
-    void DetatchMagicCircle()
-    {
-        performer.transform.parent = null;
-    }
-    
-    void Lift()
-    {
-        //Attach performer to the platform
-        AttachMagicCircle();
+        [SerializeField]
+        Transform performer;
 
-        //slowing moving up
+        [SerializeField]
+        Transform OutsideDome;
+
+        float t;
+        Vector3 newPos;
+        Vector3 originalPos;
+
+        void Start()
+        {
+            Koreographer.Instance.RegisterForEventsWithTime(eventMagicCircle, MagicEvent);
+            AttachMagicCircle();
+            originalPos = this.transform.position;
+        }
+
+        void MagicEvent(KoreographyEvent evt, int sampleTime, int sampleDelta, DeltaSlice deltaSlice)
+        {
+            if (evt.HasCurvePayload())
+            {
+                //calc new position using lerp
+                t = evt.GetValueOfCurveAtTime(sampleTime);
+
+                newPos.x = this.transform.position.x;
+                newPos.y = Mathf.Lerp(originalPos.y, OutsideDome.position.y, t);
+                newPos.z = this.transform.position.z;
+
+                //set new position
+                this.transform.position = newPos;
+            }
+        }
+
+            [Button]
+        void AttachMagicCircle()
+        {
+            performer.SetParent(this.transform, true);
+        }
+
+        [Button]
+        void DetatchMagicCircle()
+        {
+            performer.transform.parent = null;
+        }
     }
 }
